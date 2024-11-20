7f73559ddd6203fa288201db6dc2baceecd9a1f8
rowens794 <rowens794@gmail.com>
2024-11-19 20:36:36 -0500

    removed heroicons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import {
   DisclosureButton,
   DisclosurePanel,
 } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import User from "../interfaces/User";
 import { supabase } from "../lib/supabaseClient";
@@ -44,14 +44,14 @@ function Navbar({ user }: { user: User | null }) {
             <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
-              <Bars3Icon
+              {/* <Bars3Icon
                 aria-hidden="true"
                 className="block size-6 group-data-[open]:hidden"
               />
               <XMarkIcon
                 aria-hidden="true"
                 className="hidden size-6 group-data-[open]:block"
-              />
+              /> */}
             </DisclosureButton>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
